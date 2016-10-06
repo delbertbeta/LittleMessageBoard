ce5f2522c907dacb91cb5eb48644302714421c84
Delbert Zou <Delbert Zou>
2016-10-07 00:08:59 +0800

    忘记保存了= =重新Commit一下
    Signed-off-by: Delbert Zou <Delbert Zou>

--- info.php
+++ info.php
@@ -8,16 +8,18 @@
         <script src="./js/main.js"></script>
     </head>
     <body>
-        <div id="InfoDiv">
-            <img id="FaceImage" src="./assets/myface.png" />
-            <div id="UserInfoDiv">
-                <p id="UserInfo"><span id="UserInfoTitle">ID: </span>1</p>
-                <p id="UserInfo"><span id="UserInfoTitle">Username: </span>delbertbeta</p>
-                <button onclick="logout()" id="LogoutButton">LOGOUT</button>
+        <div id="ContentDiv">
+            <div id="InfoDiv">
+                <img id="FaceImage" src="./assets/myface.png" />
+                <div id="UserInfoDiv">
+                    <p id="UserInfo"><span id="UserInfoTitle">ID: </span>1</p>
+                    <p id="UserInfo"><span id="UserInfoTitle">Username: </span>delbertbeta</p>
+                    <button onclick="logout()" id="LogoutButton">LOGOUT</button>
+                </div>
             </div>
+            <svg id="Line" width="400px" height="2px" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <line x1="0" y1="0" x2="350" y2="0" style="stroke:rgb(99,99,99);stroke-width:1"></line>
+            </svg>
         </div>
-        <svg id="Line" width="400px" height="2px" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <line x1="0" y1="0" x2="350" y2="0" style="stroke:rgb(99,99,99);stroke-width:1"></line>
-        </svg>
     </body>
 </html>
